93a824d04b2b4527112fcdaed8a1320e5dc51bb1
ianl20230303 <yal299417132@gmail.com>
2026-05-19 19:34:13 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ import streamlit as st
 st.set_page_config(page_title="微型 TimeTree", layout="wide")
 
 @st.dialog("系統公告")
-    def show_alert():
-        st.write("本週作業請確認 requirements.txt 有正確設定！")
-        if st.button("查看公告"): show_alert()
+def show_alert():
+    st.write("本週作業請確認 requirements.txt 有正確設定！")
+    if st.button("查看公告"): show_alert()
 
 
 
